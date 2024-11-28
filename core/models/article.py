b5e28c2d88481b02f68501b0baa921c52961477a
Roman <kovalchuk.roman05@gmail.com>
2024-11-28 15:33:38 +0200

    article model

---
+++ core/models/article.py
@@ -0,0 +1,31 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Text, DateTime
+from datetime import datetime
+
+from .base import Base
+from .mixins import IDMixin
+
+if TYPE_CHECKING:
+    from .user import User
+
+
+class Article(IDMixin, Base):
+    __tablename__ = "articles"
+
+    title: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+
+    user: Mapped["User"] = relationship(back_populates="articles")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "created_at": self.created_at,
+            "user_at": self.user_id,
+        }
