ebadd9ced240c976f9b2a3388a4e6ed20f18cc3f
Roman <kovalchuk.roman05@gmail.com>
2024-11-30 00:46:29 +0200

    config updated

--- core/config.py
+++ core/config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 
 class Config:
+    TESTING = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
