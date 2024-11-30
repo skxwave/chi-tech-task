9fc5bf35ab5989309fce04ccb9e485907167ddbd
Roman <kovalchuk.roman05@gmail.com>
2024-11-30 14:35:50 +0200

    set default values in config

--- core/config.py
+++ core/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 class Config:
     TESTING = False
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "test_secret_key")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "test_jwt_key")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///")
