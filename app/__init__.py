bd1c06c8bfba15f33c67ee96c37090cefa7dcad5
Roman <kovalchuk.roman05@gmail.com>
2024-12-01 17:24:52 +0200

    CLI commands registered & minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from core.config import Config
-from core import db, jwt
-from .auth.views import bp as authbp
-from .articles.views import bp as articlesbp
-from .users.views import bp as usersbp
+from core import db, jwt, swagger
+from .auth import bp as authbp
+from .articles import bp as articlesbp
+from .users import bp as usersbp
+from .commands import bp as adminbp
 
 baseprefix = "/api/v1"
 
@@ -15,9 +17,12 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     jwt.init_app(app)
+    swagger.init_app(app)
+    CORS(app)
 
     app.register_blueprint(authbp, url_prefix=f"{baseprefix}/auth")
     app.register_blueprint(articlesbp, url_prefix=f"{baseprefix}/articles")
     app.register_blueprint(usersbp, url_prefix=f"{baseprefix}/users")
+    app.register_blueprint(adminbp)
 
     return app
