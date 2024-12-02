ba1621157b09ff6581e47be0a37d5303807032ad
Roman <kovalchuk.roman05@gmail.com>
2024-12-02 03:00:26 +0200

    refresh token route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from sqlalchemy import select
 
 from core import db
@@ -9,7 +9,7 @@ bp = Blueprint("auth", __name__)
 
 
 @bp.route("/login", methods=["POST"])
-def login():
+def access_token():
     data = request.get_json()
     username = data["username"]
     password = data["password"]
@@ -25,5 +25,14 @@ def login():
     if not user.password or not user.check_password(password):
         return {"msg": "Bad username or password"}, 401
 
+    access_token = create_access_token(identity=username)
+    refresh_token = create_refresh_token(identity=username)
+    return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+
+@bp.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh_token():
+    username = get_jwt_identity()
     access_token = create_access_token(identity=username)
     return {"msg": access_token}, 200
